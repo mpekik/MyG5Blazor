343ab5368ce46fddb7e9c50000b0993c3a3d7169
ucims <ucims30@gmail.com>
2020-04-29 13:34:55 +0700

    Add comands for bill

---
+++ MyG5Blazor/Data/Bill/CHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyG5Blazor.Data.Bill
+{
+    public class CHelpers
+    {
+    }
+
+    public class ChannelData
+    {
+
+    }
+}
--- MyG5Blazor/Data/BillAcceptor.cs
+++ MyG5Blazor/Data/BillAcceptor.cs
@@ -8,6 +8,14 @@ namespace MyG5Blazor.Data
 {
     public class BillAcceptor
     {
+        SSPComms m_eSSP;
+        SSP_COMMAND m_cmd;
+        SSP_KEYS keys;
+        SSP_FULL_KEY sspKey;
+        SSP_COMMAND_INFO info;
 
+        int m_ProtocolVersion;
+        int m_HoldCount;
+        //List<ChannelData> m_UnitDataList;
     }
 }
