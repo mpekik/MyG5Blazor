b26c4d8d164de99b25f2fd264e032907a421e757
mpekik <64069485+mpekik@users.noreply.github.com>
2020-05-16 14:54:48 +0700

    CES

---
+++ MyG5Blazor/Data/CES.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyG5Blazor.Data
+{
+    public class CES
+    {
+        public string quIndText = string.Empty;
+        public string quID = string.Empty;
+        public string quEngText = string.Empty;
+        public string answID = string.Empty;
+        public string answValue = string.Empty;
+        
+    }
+}
